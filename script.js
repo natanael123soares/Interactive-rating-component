939237ed8210bb1943c6f5e44696d3dcec16f5aa
natanael123soares <105888082+natanael123soares@users.noreply.github.com>
2022-06-28 20:34:59 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function mudar(){
+    const item1 = document.getElementById('avaliaçao1')
+    const item2= document.getElementById('avaliaçao2')
+    const item3 = document.getElementById('avaliaçao3')
+    const item4 = document.getElementById('avaliaçao4')
+    const item5 = document.getElementById('avaliaçao5')
+    const select = ''
+
+    switch (select){
+        case item1 :
+            item1.style.backgroundColor = '#fb7413'
+            break
+    }
+
+}
